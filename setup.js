9bb6f4874b9fdca81fca5d47fa1c872f58d8a273
OhYeah! <hidemioya@gmail.com>
2026-03-13 14:46:25 +0900

    Create setup.js

---
+++ setup.js
@@ -0,0 +1,9 @@
+// サイト名と分室名
+// FAMIC との使用許諾の関係があるので、○○分室の部分だけ適当な名称に変更
+const sitename = "携帯農薬検索実験室";
+const branch = "別室";
+
+const acfbpath = '/acfinder';
+const macspath = '/macs';
+const datapath = '/data';
+const rootsite = 'http://macs.xii.jp/';
