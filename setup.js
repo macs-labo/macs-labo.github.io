a6938d99aa4efeba9f744c9b3f1f581452f81f6a
OhYeah! <hidemioya@gmail.com>
2026-03-14 13:58:55 +0900

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,7 +1,7 @@
 // サイト名と分室名
 // FAMIC との使用許諾の関係があるので、○○分室の部分だけ適当な名称に変更
 const sitename = "携帯農薬検索実験室";
-const branch = "別室";
+const branch = "本室別館";
 
 const acfbpath = '/acfinder';
 const macspath = '/macs';
