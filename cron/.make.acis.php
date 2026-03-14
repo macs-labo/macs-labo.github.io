68b851ac9c525a863b55b20dfe0c845ea7091edd
OhYeah! <hidemioya@gmail.com>
2026-03-15 07:41:09 +0900

    Update .make.acis.php

--- cron/.make.acis.php
+++ cron/.make.acis.php
@@ -115,7 +115,7 @@ if (!$facis && !$ftoxic) return 1;
 
 // 公開 zip ファイル更新
 exec("zip -Dq $mainzip $maindb");
-rename("./$mainzip", "$datdir/$mainzip");
+rename("$mainzip", "$datdir/$mainzip");
 // 検索システム用データベース更新
 if ($dbpath) {
   mkdir("$dbpath/$udflag");
