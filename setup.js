79b4486b6d8ab5777e154ca07e6feb2655c574e7
OhYeah! <hidemioya@gmail.com>
2026-03-20 01:05:39 +0900

    Add files via upload

--- setup.js
+++ setup.js
@@ -1,7 +1,19 @@
 // サイト名と分室名
 // FAMIC との使用許諾の関係があるので、○○分室の部分だけ適当な名称に変更
 const sitename = "携帯農薬検索実験室";
-const branch = "本室別館";
+let branch = "○○分室"; //本室、秋田分室、大阪分室は自動設定するのでこのまま
+
+const branches = [
+  { host: 'macs.xii.jp', branch: '本室' },
+  { host: 'macs-labo.github.io', branch: '本室別館' },
+  { host: 'macs.kabe.info', branch: '秋田分室' },
+  { host: 'noyaku.ebb.jp', branch: '大阪分室' }
+];
+branches.forEach(b => {
+  if (window.location.host === b.host) {
+    branch = b.branch;
+  }
+});
 
 const acfbpath = '/acfinder';
 const macspath = '/macs';
